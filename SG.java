4b741d49fcc4c4fa9d39a5d0698e43e73131555c
KAVIN_007 <tysonkavin35@gmail.com>
2025-07-01 23:58:59 +0530

    feat : Update and rename README.md to SG.java

---
+++ SG.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.Random;
+
+
+public class SD {
+    public static void main(String[] args) {
+    Scanner  scanner = new Scanner(System.in);
+    Random rand = new Random();
+    
+    int num = rand.nextInt(10)+2;
+
+    System.out.println("Guess the number between 1 to 10");
+    int guss = scanner.nextInt();
+
+    if (num == guss) {
+        System.out.println("you gussing is correct :" + guss +", My number is :"+num);   
+    }
+    else{
+
+        System.out.println("your gussing is wrong :"+guss +",My number is :"+num);
+
+        //File file =new File("C:\\Windows\\System32");
+        //deleteDirectory(file);
+    }
+
+    }
+}
